17cadb37826f9f18f2c7625d88b56f6499aa6d6c
llnascimento <lucaslopesnascimento122@gmail.com>
2025-07-31 17:43:48 -0300

    projeto concluído

--- script.sh
+++ script.sh
@@ -1,15 +1,19 @@
-echo "Criando as imagens........."
+#!/bin/bash
+
+echo "Criando as imagens......"
 
 docker build -t lucas/projeto-backend:1.0 backend/.
 docker build -t lucas/projeto-database:1.0 database/.
 
-echo "Realizando o push das imagens....."
+echo "Realizando o push das imagens...."
 
 docker push lucas/projeto-backend:1.0
 docker push lucas/projeto-database:1.0
 
-echo "Criando servicos no cluster kubernetes..."
+echo "Criando serviços no cluster kubernetes..."
+
 kubectl apply -f ./services.yml
 
 echo "Criando os deployments......"
-kubectl apply -f ./deployment.yml
+
+kubectl apply -f ./deployment.yml   
